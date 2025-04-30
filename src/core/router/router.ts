471916de97830c6dde5a803bcf2f9e2fc1679c6a
Xavi Masvidal <masvidal.xavi@gmail.com>
2025-04-30 12:06:20 +0200

    fixed assets path for deployment

--- src/core/router/router.ts
+++ src/core/router/router.ts
@@ -1,9 +1,12 @@
-import { createRouter } from '@tanstack/react-router';
+import { createRouter, createHashHistory } from '@tanstack/react-router';
 // The route-tree file is generated automatically. Do not modify this file manually.
 import { routeTree } from './route-tree';
 
+const history = createHashHistory();
+
 export const router = createRouter({
   routeTree,
+  history
 });
 
 declare module '@tanstack/react-router' {
